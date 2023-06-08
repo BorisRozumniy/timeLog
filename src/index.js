a5702a0f8d8df8cbaee066ce8efaf3d6a8f76c3e
borys <borys.rozumnyi@emis-online.sk>
2023-06-08 09:10:51 +0200

    changed the index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { createTimeComputedText } from './getSpedingTime/computingSpendTime'
+
+const fs = require('fs');
+
+const pathToFile = process.argv[2]
+
+function readWriteAsync() {
+  fs.readFile(pathToFile, 'utf-8', function(err, data){
+    if (err) throw err;
+			
+    const newValue = createTimeComputedText(data);
+    fs.writeFile(pathToFile, newValue, 'utf-8', function (err) {
+      if (err) throw err;
+      console.log('filelistAsync complete');
+    });
+  });
+}
+
+readWriteAsync();
