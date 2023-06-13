2822a12c45f9658d35dc9b62357e03460b3cfbe0
borys <borys.rozumnyi@emis-online.sk>
2023-06-13 18:23:24 +0200

    moved test data to the srcTest

---
+++ src/getSpedingTime/srcTest.js
@@ -0,0 +1,43 @@
+export const text1 = 'text <!-- [2023-05-15 08:12] [2023-05-15 08:20] -->'
+export const text2 =
+	'- [ ] GP-5853 <!-- [2023-05-16 08:10][2023-05-16 08:14] [2023-05-16 08:39][2023-05-16 08:42] [2023-05-16 08:55][2023-05-16 09:01]-->' // 13m
+export const text3 =
+	' [ ] GP-5853 <!-- [2023-05-16 08:10][2023-05-16 08:14] [2023-05-16 08:39][2023-05-16 08:42] [2023-05-16 08:55]-->'
+export const text4 =
+	' [ ] GP-5853 <!-- [2023-05-16 08:10]-->'
+export const text5 = `
+## 2023-05-17
+- [x] GP-5828 fix for the missed category <!-- [2023-05-17 07:02][2023-05-17 08:55] [2023-05-17 09:09][2023-05-17 09:18]-->
+- [ ] HP GP-5818 - need to change content for golf page with KIn <!-- [2023-05-17 10:51][2023-05-17 11:22] -->
+  - [x]  added the golfPage to the locales
+  - [ ]  deploy to dev <!-- [2023-05-17 11:55][2023-05-17 13:36] -->
+- [ ] timer [2023-05-17 09:24][2023-05-17 10:50]
+- [ ] GP-5853 <!-- [2023-05-17 14:05][2023-05-17 14:59] -->
+  - [ ] try to understand where is bug
+`
+export const text6 = `
+## 2023-05-17
+- [x] GP-5828 fix for the missed category <!-- [2023-05-17 07:02][2023-05-17 08:55] [2023-05-17 09:09][2023-05-17 09:18] [2023-05-17 09:20][2023-05-17 09:30] --> **2h 2m**
+- [ ] HP GP-5818 - need to change content for golf page with KIn <!-- [2023-05-17 10:51][2023-05-17 11:22] --> **31m**
+`
+	
+export const resultOf_getTeims1 = ['2023-05-15 08:12', '2023-05-15 08:20'] // 8
+export const resultOf_getTeims2 = ['2023-05-16 08:10', '2023-05-16 08:14', '2023-05-16 08:39', '2023-05-16 08:42', '2023-05-16 08:55', '2023-05-16 09:01'] // 13m
+export const resultOf_getTeims3 = ['2023-05-16 08:10', '2023-05-16 08:14', '2023-05-16 08:39', '2023-05-16 08:42', '2023-05-16 08:55']
+export const resultOf_getTeims4 = ['2023-05-16 08:10']
+export const resultOf_getcreateTimeComputedText1 = "- [ ] GP-5853 <!-- [2023-05-16 08:10][2023-05-16 08:14] [2023-05-16 08:39][2023-05-16 08:42] [2023-05-16 08:55][2023-05-16 09:01]--> **13m**"
+export const resultOf_getcreateTimeComputedText2 = `
+## 2023-05-17
+- [x] GP-5828 fix for the missed category <!-- [2023-05-17 07:02][2023-05-17 08:55] [2023-05-17 09:09][2023-05-17 09:18]--> **2h 2m**
+- [ ] HP GP-5818 - need to change content for golf page with KIn <!-- [2023-05-17 10:51][2023-05-17 11:22] --> **31m**
+  - [x]  added the golfPage to the locales
+  - [ ]  deploy to dev <!-- [2023-05-17 11:55][2023-05-17 13:36] --> **1h 41m**
+- [ ] timer [2023-05-17 09:24][2023-05-17 10:50] **1h 26m**
+- [ ] GP-5853 <!-- [2023-05-17 14:05][2023-05-17 14:59] --> **54m**
+  - [ ] try to understand where is bug
+`
+export const resultOf_getcreateTimeComputedText3 = `
+## 2023-05-17
+- [x] GP-5828 fix for the missed category <!-- [2023-05-17 07:02][2023-05-17 08:55] [2023-05-17 09:09][2023-05-17 09:18] [2023-05-17 09:20][2023-05-17 09:30] --> **2h 12m**
+- [ ] HP GP-5818 - need to change content for golf page with KIn <!-- [2023-05-17 10:51][2023-05-17 11:22] --> **31m**
+`
